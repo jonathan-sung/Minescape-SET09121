c68561943263fb10003bb2a92896713d3efd366a
jonathan-sung <60101351+jonathan-sung@users.noreply.github.com>
2021-05-02 17:35:17 +0100

    Revert "Merge pull request #20 from jonathan-sung/Scoring"
    This reverts commit b625ccab3c5665446b39986dedf6e5ee36f6e6d1, reversing
    changes made to 9a1f272b7bc124fa1cadacb85f4112399ff0ef11.

--- minescape/components/cmp_rope.h
+++ minescape/components/cmp_rope.h
@@ -14,9 +14,10 @@ private:
 	float launchSpeed;
 	float ropeCurrentLength;
 	float ropeMaxLength;
-	float impulse;
 	float impulseTime;
 	float impulseTimer;
+	Vector2f forceImpulse; 
+	Vector2f addedImpulse;
 
 	Vector2f initialRopePosition;
 	Vector2f currentEndPointPosition;
